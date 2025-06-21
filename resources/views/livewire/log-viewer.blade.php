99ff566046fe343e8330f906b5e2e0dfc2844c12
Sundari <daripray.dev@gmail.com>
2025-06-21 13:26:08 +0700

    Update log-viewer.blade.php

--- resources/views/livewire/log-viewer.blade.php
+++ resources/views/livewire/log-viewer.blade.php
@@ -40,7 +40,8 @@
                                     data-bs-toggle="modal" 
                                     data-bs-target="#videoModal" 
                                     data-video="{{ asset('storage/' . $video['path']) }}" 
-                                    data-log="{{ $video['name'] }}">
+                                    data-name="{{ $video['name'] }}" 
+                                    data-log="{{ $video['log_detail']['datetime'] }}">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                                         <path d="m380-300 280-180-280-180v360ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
                                     </svg>
@@ -75,7 +76,7 @@
         <div class="modal-dialog modal-lg modal-dialog-centered">
             <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Tampilan Video</h5>
+                <h5 id="modalTitle" class="modal-title w-100 text-center">Tampilan Video</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Tutup"></button>
             </div>
             <div class="modal-body text-center">
@@ -92,17 +93,20 @@
 
 <script>
     const videoModal = document.getElementById('videoModal');
+    const modalTitle = document.getElementById('modalTitle');
     const modalVideo = document.getElementById('modalVideo');
     const modalLogDetail = document.getElementById('modalLogDetail');
 
     videoModal.addEventListener('show.bs.modal', function (event) {
         const button = event.relatedTarget;
+        const videoName = button.getAttribute('data-name');
         const videoSrc = button.getAttribute('data-video');
         const logDetail = button.getAttribute('data-log');
 
         modalVideo.querySelector('source').setAttribute('src', videoSrc);
         modalVideo.load();
         modalLogDetail.textContent = logDetail || 'Log tidak tersedia';
+        modalTitle.textContent = videoName || '';
     });
 
     videoModal.addEventListener('hidden.bs.modal', function () {
